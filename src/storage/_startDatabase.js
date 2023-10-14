6a891bf2dc7f35af5675b84eecfbba4e320a511a
Lucas Costa <loucongxd@gmail.com>
2023-10-14 13:23:33 -0400

    fix/ created script for db start and reset

--- src/storage/_startDatabase.js
+++ src/storage/_startDatabase.js
@@ -1,9 +1,10 @@
-const { getConnection } = require('./index');
+const { getConnection } = require("./index");
 
 async function start() {
-    const db = await getConnection();
+  const db = await getConnection();
 
-    // User Table
+  // User Table
+  try {
     await db.exec(`
         CREATE TABLE IF NOT EXISTS users (
             id       INTEGER PRIMARY KEY,
@@ -12,10 +13,14 @@ async function start() {
             password TEXT NOT NULL
         )
     `);
-    await db.exec('CREATE UNIQUE INDEX users_username ON users(username)');
-    await db.exec('CREATE UNIQUE INDEX users_email ON users(email)');
-        }
-
+    await db.exec("CREATE UNIQUE INDEX users_username ON users(username)");
+    await db.exec("CREATE UNIQUE INDEX users_email ON users(email)");
+  } catch (err) {
+    if(err?.message?.startsWith("SQLITE_ERROR: index users_username already exists")){
+        console.log("Database already exists!!");
+    }
+  }
+}
 (async function () {
-    await start();
+  await start();
 })();
